cda35eee451c4521ed4e6376f69e8a129968a4b4
Xintong Zhao <xintong.zhao377@outlook.com>
2026-01-05 20:22:04 -0800

    update

--- 1004MaxConsecutiveOnesIII.py
+++ 1004MaxConsecutiveOnesIII.py
@@ -4,16 +4,13 @@ class Solution:
         l = 0
         zero_count = 0
         ans = 0
-
         for r in range(len(nums)):
             if nums[r] == 0:
                 zero_count += 1
-
             while zero_count > k:
                 if nums[l] == 0:
                     zero_count -= 1
                 l += 1
-
             ans = max(ans, r - l + 1)
 
         return ans
